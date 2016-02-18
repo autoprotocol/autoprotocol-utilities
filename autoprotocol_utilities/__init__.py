719948708652e4bd18a30860f50cf2d708058ab2
Cornelia Scheitz <cornelia.scheitz@gmail.com>
2016-02-18 12:32:43 -0800

    fixing init

--- autoprotocol_utilities/__init__.py
+++ autoprotocol_utilities/__init__.py
@@ -1,5 +1,5 @@
-from .container_helpers import volume_check, set_pipettable_volume, det_new_group, plates_needed, sort_well_group, unique_containers, is_columnwise, stamp_shape, first_empty_well, list_of_filled_wells
-from .misc_helpers import user_errors_group, char_limit, printdatetime, printdate, make_list, flatten_list
+from .container_helpers import volume_check, set_pipettable_volume, plates_needed, sort_well_group, unique_containers, is_columnwise, stamp_shape, first_empty_well, list_of_filled_wells
+from .misc_helpers import user_errors_group, char_limit, printdatetime, printdate, make_list, flatten_list, det_new_group
 from .modules import createMastermix, autoseal, serial_dilute_rowwise
-from .resource_helpers import ResourceIDs, scale_default, return_dispense_media, return_agar_plates, ref_kit_container
+from .resource_helpers import ResourceIDs, oligo_scale_default, return_dispense_media, return_agar_plates, ref_kit_container
 from .thermocycle_helpers import melt_curve, thermocycle_ramp
